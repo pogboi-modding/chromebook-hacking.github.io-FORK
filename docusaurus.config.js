b60aaecc972e27a190a364cedca7625e531fb915
xytrux <helloworldpy103@gmail.com>
2024-11-23 15:47:13 -0800

    capitalize C

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Chromebook Exploit Guide',
-  tagline: 'An updated exploit guide and wiki for a wide variety of chromebook exploits and tools.',
+  tagline: 'An updated exploit guide and wiki for a wide variety of Chromebook exploits and tools.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
